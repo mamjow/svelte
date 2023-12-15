b42ae827258314f213bb071571640cb456e0b39f
M.J. <mj.moshiri@gmail.com>
2023-12-15 10:16:42 +0100

    mvp

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -7,8 +7,8 @@ export default defineConfig(() => {
 		server: {
 			proxy: {
 				'/api': {
-					target : 'http://localhost:5233',
-					changeOrigin: false,
+					target : 'http://api.moshiri.nl',
+					changeOrigin: true,
 					rewrite: (path) => path.replace(/^\/api/, ''),
 					secure: false,
 				},		
